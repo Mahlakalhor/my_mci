d9d9eae918ba6b13dac11a6b13e090c236df63e6
Mahla <Mahlakalhor82@gmail.com>
2025-11-30 14:21:50 +0330

    v1

---
+++ src/i18n/index.ts
@@ -0,0 +1,15 @@
+import i18n from "i18next";
+import { initReactI18next, Translation } from "react-i18next";
+
+const resources = {
+  en: {
+    Translation: {
+      "": "",
+    },
+  },
+  fa: {
+    Translation: {
+      "": "",
+    },
+  },
+};
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -22,46 +22,42 @@ export const Login = () => {
         <button className="bg-white text-black flex items-center justify-center rounded-lg h-14 p-2">
           ورود با رمز عبور
         </button>
-        <app-icon
-          _ngcontent-ng-c4278918718=""
-          svgicon="login_by_password"
-          classlist="login-icons-stroke flex !h-5 !w-5 !text-xl"
+        <svg
+          className="w-5 h-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <mat-icon
-            role="img"
-            class="mat-icon notranslate !h-5 !text-xl !w-5 flex login-icons-stroke mat-icon-no-color"
-            aria-hidden="true"
-            data-mat-icon-type="svg"
-            data-mat-icon-name="login_by_password"
-          >
-            <svg
-              fill="none"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              fit=""
-              height="100%"
-              width="100%"
-              preserveAspectRatio="xMidYMid meet"
-              focusable="false"
-            >
-              <defs></defs>
-              <path
-                stroke="#ff4f00"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="1.5"
-                d="M8.333 11.833H6M6.342 13l1.65-2.333m-1.65 0L7.992 13M13.167 11.833h-2.333M11.176 13l1.65-2.333m-1.65 0L12.826 13M18 11.833h-2.334M16.008 13l1.65-2.333m-1.65 0L17.658 13"
-              ></path>
-              <path
-                stroke="#0095da"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="1.5"
-                d="M21 15V9a3 3 0 00-3-3H6a3 3 0 00-3 3v6a3 3 0 003 3h12a3 3 0 003-3z"
-              ></path>
-            </svg>
-          </mat-icon>
-        </app-icon>
+          <path
+            stroke="black"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
+            d="M8.333 11.833H6 M6.342 13l1.65-2.333m-1.65 0L7.992 13"
+          />
+          <path
+            stroke="black"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
+            d="M13.167 11.833h-2.333 M11.176 13l1.65-2.333m-1.65 0L12.826 13"
+          />
+          <path
+            stroke="black"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
+            d="M18 11.833h-2.334 M16.008 13l1.65-2.333m-1.65 0L17.658 13"
+          />
+
+          <path
+            stroke="black"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
+            d="M21 15V9a3 3 0 00-3-3H6a3 3 0 00-3 3v6a3 3 0 003 3h12a3 3 0 003-3z"
+          />
+        </svg>
       </div>
     </div>
   );
